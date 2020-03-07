9aeab3b560b3349d84629dc90d51696d8213f8fa
Kilo <>
2020-03-07 18:44:50 +0300

    Initial commit

---
+++ src/main/java/com/assessment/insuranceprofile/InsuranceProfileApplication.java
@@ -0,0 +1,13 @@
+package com.assessment.insuranceprofile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceProfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsuranceProfileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assessment/insuranceprofile/InsuranceProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assessment.insuranceprofile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceProfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
